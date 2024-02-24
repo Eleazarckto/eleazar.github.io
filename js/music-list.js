77c74a0c3eef03637b3ac9fd93ce4b34d73cb5b0
Eleazarckto <124305575+Eleazarckto@users.noreply.github.com>
2024-02-24 10:23:06 -0600

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+  {
+    name: "Youngblood",
+    artist: "5 Seconds Of Summer",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Wake Me Up",
+    artist: "Avicii",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Locked Out Of Heaven",
+    artist: "Bruno Mars",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Galway Girl",
+    artist: "Ed Sheeran",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Hoping For Snow",
+    artist: "The Vamps",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Immortals",
+    artist: "Fall Out Boy",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Can We Dance",
+    artist: "The Vamps",
+    img: "music-7",
+    src: "music-7"
+  },
+
+];
